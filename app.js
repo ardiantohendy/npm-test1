ed7a89bca3705005333260d49a8d6183a3cdeca7
Ardiantohendy <bayuardianto421@gmail.com>
2022-10-04 23:51:17 +0700

    my update

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const fs = require("fs");
 const validator = require("validator");
 const Readline = require("readline");
+const { exit } = require("process");
+// const allContact = [];
+const file = fs.readFileSync("data/contacts.json", "utf-8");
+const contacts = JSON.parse(file);
 
 const rl = Readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const goToMainMenu = () => {
+  rl.question("1. Exit\n\t", (exit) => {
+    if (exit == 1) mainMenu();
+  });
+};
+
 const validation = () => {
-  rl.question("1. Add new contact\n2. Exit\nYour answer (1 or 2): ", (number) => {
+  rl.question("1. Add new contact\n2. Main menu\n3. Exit\nYour answer (1 or 2...): ", (number) => {
     if (number == 1) {
       newContact();
+    } else if (number == 2) {
+      mainMenu();
     } else {
       rl.close();
     }
@@ -28,8 +40,6 @@ const newContact = () => {
               phoneNumber,
               email,
             };
-            const file = fs.readFileSync("data/contacts.json", "utf-8");
-            const contacts = JSON.parse(file);
             contacts.push(person);
             fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
             console.log("Contact successfully added to phone!!!");
@@ -47,4 +57,48 @@ const newContact = () => {
   });
 };
 
-newContact();
+const getAllContact = () => {
+  for (let i = 0; i < contacts.length; i++) {
+    console.log("Name: " + contacts[i].name + "\nPhone Number: " + contacts[i].phoneNumber + "\nEmail: " + contacts[i].email + "\n********************************************************");
+  }
+};
+
+const searchContactName = () => {
+  let isExist = true;
+  rl.question("Search contact nama: ", (nameContact) => {
+    for (let i = 0; i < contacts.length; i++) {
+      if (nameContact.toLocaleLowerCase() === contacts[i].name.toLocaleLowerCase()) {
+        console.log("Name: " + contacts[i].name + "\nPhone Number: " + contacts[i].phoneNumber + "\nEmail: " + contacts[i].email + "\n********************************************************");
+        isExist = false;
+      }
+    }
+    if (isExist) {
+      console.log("There's no contact named " + nameContact + " in your phone");
+      rl.question("1. Search again\n2. Back to main menu\t", (exit) => {
+        if (exit == 1) {
+          searchContactName();
+        } else {
+          mainMenu();
+        }
+      });
+    }
+    goToMainMenu();
+  });
+};
+
+const mainMenu = () => {
+  rl.question("1. Add new contact\n2. Get all contact\n3. Search contact\n4. Exit\n\t", (mainOptions) => {
+    if (mainOptions == 1) {
+      newContact();
+    } else if (mainOptions == 2) {
+      getAllContact();
+      goToMainMenu();
+    } else if (mainOptions == 3) {
+      searchContactName();
+    } else if (mainOptions == 4) {
+      rl.close();
+    }
+  });
+};
+
+mainMenu();
